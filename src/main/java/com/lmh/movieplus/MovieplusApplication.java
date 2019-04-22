74795860cb7af5191ac0cff72bfee8d24d4489da
lmhwq <miaohua655@yahoo.com>
2019-04-22 14:00:48 +0800

    lmh

---
+++ src/main/java/com/lmh/movieplus/MovieplusApplication.java
@@ -0,0 +1,13 @@
+package com.lmh.movieplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieplusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lmh/movieplus/MovieplusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lmh.movieplus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieplusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
